ad847bc85e5819014f51c2034dd5bcc06ceb1d80
Koichiro12 <choiruldhani889@gmail.com>
2024-07-10 21:05:53 +0700

    Setup Theme

---
+++ resources/views/partials/Footer.blade.php
@@ -0,0 +1,9 @@
+
+<script src="{{ asset('/') }}assets/static/js/components/dark.js"></script>
+<script src="{{ asset('/') }}assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="{{ asset('/') }}assets/compiled/js/app.js"></script>
+<script src="{{ asset('/') }}assets/extensions/apexcharts/apexcharts.min.js"></script>
+<script src="{{ asset('/') }}assets/static/js/pages/dashboard.js"></script>
+@yield('content-script')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Auth\ForgetPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login/auth', [LoginController::class, 'auth'])->name('auth');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'register'])->name('auth_register');
+
+Route::group(['middleware' => ['auth', 'prevent-back']], function () {
+    
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/profile', [DashboardController::class, 'profile'])->name('profile');
+    Route::post('/profile/update', [DashboardController::class, 'updateProfile'])->name('updateProfile');
+
 });
