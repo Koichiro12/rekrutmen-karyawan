8d14d8d211fa1aa89b0262bf98e7a0a6971deb39
Koichiro12 <choiruldhani889@gmail.com>
2024-07-18 18:43:29 +0700

    update

--- app/Http/Controllers/User/PersonalDataController.php
+++ app/Http/Controllers/User/PersonalDataController.php
@@ -8,4 +8,12 @@ use Illuminate\Http\Request;
 class PersonalDataController extends Controller
 {
     //
+
+    public function index(){
+        $data = $this->getPageData();
+        $data['page_name'] = 'Personal Data';
+        $data['page_subname'] = 'Personal Data data will appear here';
+        $data['page_breadcum'] = array_merge($data['page_breadcum'],[['name' => 'Personal Data','link' => route('experiences.index'),'status' => 'active']]);
+        return view('pages.user.personaldata.index',compact(['data']));
+    }
 }
