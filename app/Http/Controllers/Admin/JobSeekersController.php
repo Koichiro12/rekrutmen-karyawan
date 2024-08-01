f90f5fa6684b37310ad39aa63b7bf4b4af2787a9
Koichiro12 <choiruldhani889@gmail.com>
2024-08-01 19:38:16 +0700

    update

--- app/Http/Controllers/Admin/JobSeekersController.php
+++ app/Http/Controllers/Admin/JobSeekersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ApplyJobs;
 use Illuminate\Http\Request;
 
 class JobSeekersController extends Controller
@@ -13,6 +14,7 @@ class JobSeekersController extends Controller
         $data['page_name'] = 'JobSeekers';
         $data['page_subname'] = 'JobSeekers data will appear here';
         $data['page_breadcum'] = array_merge($data['page_breadcum'],[['name' => 'JobSeekers','link' => route('jobseekers'),'status' => 'active']]);
-        return view('pages.admin.jobseekers.index',compact(['data']));
+        $jobseekers = ApplyJobs::join('jobs','apply_jobs.job_id','=','jobs.id')->join('job_seekers','apply_jobs.jobseeker_id','=','job_seekers.id')->join('departements','jobs.departement_id','=','departements.id')->join('positions','jobs.position_id','=','positions.id')->orderBy('apply_jobs.created_at','DESC')->get();
+        return view('pages.admin.jobseekers.index',compact(['data','jobseekers']));
     }
 }
--- resources/views/pages/Admin/Jobseekers/index.blade.php
+++ resources/views/pages/Admin/Jobseekers/index.blade.php
@@ -26,10 +26,52 @@
                                 <thead>
                                     <tr>
                                         <th>No</th>
+                                        <th>Tanggal</th>
+                                        <th>Lowongan</th>
+                                        <th>Pelamar</th>
+                                        <th>Status</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
+                                <tbody>
+                                    @foreach ($jobseekers as $item)
+                                        <tr>
+                                            <td>{{$loop->index + 1}}</td>
+                                            <td>{{date_format(date_create($item->date_apply),'d M,Y')}}</td>
+                                            <td>{{$item->job_name}} <br>
+                                                <small>{{date_format(date_create($item->open_date),'d M,Y')}} - {{date_format(date_create($item->Dclose_date),'d M,Y')}}</small>
+                                                <br>
+                                                <small>Departement : {{$item->departement}}</small><br>
+                                                <small>Poisition : {{$item->position}}</small>
+                                            </td>
+                                            <td>
+                                                Name : {{$item->name}} <br>
+                                                Email : {{$item->email}} <br>
+                                                Phone : {{$item->phone_number}} <br>
+                                            </td>
+                                            <td>
+                                                @switch($item->status_apply)
+                                                @case(0)
+                                                    <span class="badge bg-warning">Waiting Confirmation</span>
+                                                @break
 
+                                                @case(1)
+                                                    <span class="badge bg-success">Pass Selection</span>
+                                                @break
+
+                                                @case(2)
+                                                    <span class="badge bg-danger">Failed Selection</span>
+                                                @break
+
+                                                @default
+                                            @endswitch
+                                            </td>
+                                            <td>
+                                                <a href="#" class="btn btn-sm btn-primary">Detail</a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
                             </table>
                         </div>
                     </div>
